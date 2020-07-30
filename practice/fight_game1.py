954c09403a2646747a2c636943870c19c2a2970c
Lyn <1242458168@qq.com>
2020-07-30 20:20:29 +0800

    回合制游戏

--- practice/fight_game1.py
+++ practice/fight_game1.py
@@ -1,7 +1,7 @@
 """
 一个回合制游戏，每个角色都有hp 和power，
 hp代表血量，power代表攻击力，hp的初始值为1000，
-power的初始值为200。打斗多个回合
+power的初始值为100。打斗多个回合
 定义一个fight方法：
 my_hp = hp - enemy_power
 enemy_final_hp = enemy_hp - my_power
